56a1fa8702cee97ddfecc606b6000d7b796b323c
juarezjunior256 <juarezjunior.live@gmail.com>
2021-05-27 00:18:50 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -6,18 +6,21 @@ const form = document.querySelector('#form')
 const inputTransactionName = document.querySelector('#text')
 const inputTransactionAmount = document.querySelector('#amount')
 
-let dummyTransactions = [
-    {id:1, name: "seguro carro", amount: -500},
-    {id:2, name: "salário", amount: 1500},
-    {id:3, name: "peneu", amount: -300}
-]
+// busca no localStorage transações
+const localStorageTransaction = JSON.parse(localStorage.getItem('transactions'))
+// se localStorage tiver transações, irá buscar as transações
+// se não tiver, inicia com um array vazio
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransaction : []
 
+// remove a transação selecionada
 const removeTransaction = ID => {
-    dummyTransactions = dummyTransactions.filter(transaction => transaction.id !== ID)
+    transactions = transactions.filter(transaction => transaction.id !== ID)
+    
+    updateLocalStorage()
     init()
 }
 
-// lista de receitas e despesas
+// adiciona no Dom as transações 
 const addTransactionsIntoDOM = transaction => {
     const operator = transaction.amount < 0 ? '-' : '+'
     const cssClass = transaction.amount  < 0 ? 'minus' : 'plus'
@@ -36,10 +39,14 @@ const addTransactionsIntoDOM = transaction => {
 
 // atuliza o display das receitas 
 const updateBalanceValues = () => {
-    const transactionsAmounts = dummyTransactions.map(transaction =>  transaction.amount)
+    // mapea os valores das transações 
+    const transactionsAmounts = transactions.map(transaction =>  transaction.amount)
+    // calcula todas receitas e depesas 
     const total = transactionsAmounts .reduce((acc, transaction) => acc + transaction,  0).toFixed(2)
+    // soma todas as receitas positivas 
     const income = transactionsAmounts.filter(value => value > 0)
         .reduce((acc , value) => acc + value, 0).toFixed(2)
+     // soma todas despesas negativas    
     const expense = Math.abs(transactionsAmounts.filter(value => value < 0)
         .reduce((acc , value) => acc + value, 0)).toFixed(2)
     
@@ -48,31 +55,44 @@ const updateBalanceValues = () => {
     expenseDisplay.textContent = `R$ ${expense}`
 }
 
+// função de inicialização
 const init = () => {
     transactionUl.innerHTML = ''
-    dummyTransactions.forEach(addTransactionsIntoDOM)
+    transactions.forEach(addTransactionsIntoDOM)
     updateBalanceValues()
 }
 
 init()
 
+// inserindo no localStorage
+const updateLocalStorage = () => {
+    localStorage.setItem('transaction', JSON.stringify(transactions))
+}
+
+// irá gerar um id randomico entre 1 a 1000
 const generateID = () => Math.round(Math.random() * 1000)
 
 form.addEventListener('submit', event => {
     event.preventDefault()
 
+    // recebendo valores dos campos 
     const transactionName = inputTransactionName.value.trim()
     const transactionsAmounts = inputTransactionAmount.value.trim()
 
+    // condição para verificar valores dos campos, caso seja nulo em algum, irá disparar um alerta
     if (inputTransactionName.value.trim() === '' || inputTransactionAmount.value.trim() === '') {
         alert('Por favor, preenchar os campos')
         return
     }
 
+    // criando o objeto transação
     const transaction =  {id: generateID(), name: transactionName, amount: Number(transactionsAmounts)}
-    dummyTransactions.push(transaction)
+    //inserindo a trasanção no array
+    transactions.push(transaction)
     init()
+    updateLocalStorage()
 
+    // limpando campos depois inserir a transação 
     inputTransactionName.value = ''
     inputTransactionAmount.value = ''
 })
